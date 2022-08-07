d61346ceeda94ebe566ec1f71defcbc2c0ee696f
Augustine Love <augustinelovestephens@gmail.com>
2022-08-07 04:17:35 +0000

    Prints prime numbers below an input

---
+++ main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+    
+class Main{
+    static void isPrime() {
+        int number;
+        int sum=0;
+        float count=0;
+        float average;
+        System.out.print("Please enter number: ");
+        Scanner input=new Scanner(System.in);
+        number=input.nextInt();
+        System.out.println("*******************************************");
+        System.out.println("Prime numbers below "+number+" are");
+        for(int x=2; x<number;x++){
+            int flag=0;
+            for(int i=2; i<=x/2; i++){
+                if(x%i==0){
+                    flag=1; break;
+                }
+            }
+            if(flag==0){
+                System.out.println(x);
+                sum=sum+x;
+                count+=1;
+            input.close();
+            }
+        }
+        System.out.println("*******************************************");
+        average=sum/count;
+        System.out.println("The count of prime numbers below "+number+" is "+count);
+        System.out.println("___________________________________________");
+        System.out.println("The sum of prime numbers printed is "+sum);
+        System.out.println("___________________________________________");
+        System.out.println("The average of the prime numbers is "+average);
+        System.out.println("___________________________________________");
+    }
+    public static void main(String[] args){
+      isPrime();
+    }
+}
